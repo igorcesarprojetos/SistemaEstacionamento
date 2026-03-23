15e4c44920a844b880f66f4e86fb8e4582b1e82e
igorcesarprojetos <igorcesarprojetos@gmail.com>
2026-03-22 21:45:45 -0300

    ajustes em create e edição de estacionamento

--- SistemaEstacionamento.Main/obj/Debug/net10.0/SistemaEstacionamento.Main.AssemblyInfo.cs
+++ SistemaEstacionamento.Main/obj/Debug/net10.0/SistemaEstacionamento.Main.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaEstacionamento.Main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34a5e3491fb8e78b43a2e82f152be42a7b513bc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f47a9782d06d279696ac0d84407636c3b89985b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaEstacionamento.Main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaEstacionamento.Main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
